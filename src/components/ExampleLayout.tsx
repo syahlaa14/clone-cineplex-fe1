c755fe5d40eb7820dababd73be670a3c399b0945
snoorlax <rivo.putra@suretybond.co.id>
2023-03-21 16:00:17 +0700

    remove unused navbar

--- src/components/ExampleLayout.tsx
+++ src/components/ExampleLayout.tsx
@@ -10,12 +10,6 @@ const ExampleLayout = ({ children }: any) => {
       <div className="w-full bg-gray-50 h-[100px] pb-[48px] md:pb-0 px-2 md:px-5">
         Footer
       </div>
-      <div className="fixed bottom-0 z-50 w-full bg-white h-[70px] p-2 border border-t md:hidden">
-        <div className="flex justify-evenly">
-          <Link href="/">Home</Link>
-          <Link href="/page2">Page 2</Link>
-        </div>
-      </div>
     </>
   );
 };
