54bef9b9a142549a0915599882f74ff912d6327a
DELL <syahlakheisya14@gmail.com>
2023-03-24 09:30:57 +0700

    update

--- src/components/ExampleLayout.tsx
+++ src/components/ExampleLayout.tsx
@@ -5,7 +5,7 @@ import {} from "@fortawesome/react-fontawesome";
 const ExampleLayout = ({ children }: any) => {
   return (
     <>
-      <div className="fixed z-67 w-full bg-secondary h-[85px] p-5 border border-b">
+      <div className="flex z-67 w-full bg-secondary h-[85px] p-5 border border-b">
         <Image
           style={{ marginLeft: "50px" }}
           src="/images/logo.png"
@@ -15,7 +15,7 @@ const ExampleLayout = ({ children }: any) => {
           height={100}
         />
       </div>
-      <div className="pt-[105px] pb-3 px-2 md:px-5 flex space-between bg-tiga">
+      <div className="pt-[15px] w-full h-[55px] pb-3 px-2 md:px-5 flex space-between bg-tiga">
         <ul>
           <a href="#" className="flex-items-center">
             <Image
@@ -45,7 +45,7 @@ const ExampleLayout = ({ children }: any) => {
               style={{ float: "right", margin: "0rem 5rem 1rem 1rem" }}
             />
           </a>
-          <a href="#" className="p-3 text-green-700">
+          <a href="#" style={{ margin: "50px" }} className="p-3 text-green-700">
             Now Playing
           </a>
           <a href="#" className="p-3 text-green-700">
@@ -63,13 +63,35 @@ const ExampleLayout = ({ children }: any) => {
         </ul>
       </div>
       <Image
-        style={{ marginLeft: "70px" }}
+        style={{ margin: "35px 100px" }}
         src="/images/iklan.png"
-        alt="logo"
+        alt="iklan"
         width={865}
         height={100}
       />
-      <div className="full pt-[13px] pb-4 px-2 md:px-5">{children}</div>
+      <div
+        className="md:px-5  bg-secondary text-gray-50"
+        style={{
+          margin: "50px 70px",
+          display: "inline-block",
+          padding: "6px",
+          border: "2px",
+        }}
+      >
+        <p>Now Playing</p>
+      </div>
+      <div
+        className="md:px-5 bg-gray-300 text-green-500"
+        style={{
+          marginLeft: "0px",
+          display: "inline-block",
+          padding: "6px",
+          border: "2px",
+        }}
+      >
+        <p>Jakarta</p>
+      </div>
+      <div className="full pt-[10px] pb-2 px-10 md:px-5">{children}</div>
       <div className="w-full bg-secondary h-[100px] pb-[48px] md:pb-0 px-2 md:px-5">
         Footer
       </div>
